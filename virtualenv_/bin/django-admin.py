5157ee72f82058d36a5d3a74ccebe9cb0f97784c
Kevin De Asis <deasis@ualberta.ca>
2015-03-04 22:14:43 -0700

    added meeting minutes, removed virtual environment

--- virtualenv_/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/kevindeasis/webproject/cmput410-project/cmput410-project/virtualenv_/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
