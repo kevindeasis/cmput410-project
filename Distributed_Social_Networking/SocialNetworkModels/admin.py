c8bf58e836277f0c932e1e5ab00a372b840e3c5e
Glenn Meyer <gmeyer1@ualberta.ca>
2015-03-11 00:04:48 -0700

    updated user administration

--- Distributed_Social_Networking/SocialNetworkModels/admin.py
+++ Distributed_Social_Networking/SocialNetworkModels/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 #from SocialNetworkModels.models import Author, Posts
 from SocialNetworkModels.models import Author, Posts, Friends, FriendManager, Follows, FollowManager, FriendManager
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
+class AuthorInLine(admin.StackedInline):
+    model = Author
+    list_display = ('approved')
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [AuthorInLine]
+    list_display = ('username', 'email', 'first_name', 'last_name', 'approved')
+
+    def approved(self, obj):
+        return obj.author.approved
+    approved.admin_order_field = 'approved'
+    approved.boolean = True
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Follows)
-admin.site.register(Author)
 admin.site.register(Posts)
 admin.site.register(Friends)
